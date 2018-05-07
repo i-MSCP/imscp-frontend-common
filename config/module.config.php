1330b56428d07b56ee7ebb1f4b669b178b2dfb35
Laurent Declercq <l.declercq@nuxwin.com>
2018-05-07 14:34:35 +0200

    Wrong namespace

--- config/module.config.php
+++ config/module.config.php
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-namespace iMSCP\FrontendCommon;
+namespace iMSCP\Frontend\Common;
 
 return [
     'session_containers' => [
